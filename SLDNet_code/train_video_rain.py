22418db3181e72cdb022d2c5ccfa83ba20356d11
Yang Wenhan <yangwenhan@pku.edu.cn>
2021-04-11 13:21:21 +0800

    Update three packages in networks

--- SLDNet_code/train_video_rain.py
+++ SLDNet_code/train_video_rain.py
@@ -26,6 +26,9 @@ from loss import *
 from option import *
 import torch.nn.functional as F
 
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+
 def mask_generate(frame1, frame2, b, c, h, w, th):
     img_min = torch.min(frame1, dim=1)
     img_min = img_min[0]
